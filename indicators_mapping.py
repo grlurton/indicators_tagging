7aff91bd9465cacff309d4b62602058ac0721783
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-04-12 16:37:49 -0700

    adding getting function to get indicators dimension

--- indicators_mapping.py
+++ indicators_mapping.py
@@ -11,16 +11,6 @@ ethiopia = data['ethiopia']
 kivu = data['kivu']
 rca = data['rca']
 
-
-def get_indicator(indicator1) :
-    """Gets an indicator and looks into it.
-    Args:
-        indicator1: Indicator we want to look into.
-    """
-    print(indicator1)
-
-
-
 def measure_dimension_distance( indicator1 , indicator2 , dimension , distance):
     """Measures disance in given dimension between two indicators
 
@@ -30,4 +20,34 @@ def measure_dimension_distance( indicator1 , indicator2 , dimension , distance):
     distance_indics = (dim1 != dim2)*distance
     return distance_indics
 
+def get_indicator_dimension(indicator):
+    """Gets the dimensions in a given indicator.
+
+    Gets the different tagging dimensions in an indicator and returns them as a
+    list.
+
+    Args:
+        indicator: an indicator for which we want to know distance
+
+    Returns:
+        A list of dimensions of indicator
+    """
+    keys = {}
+    for key in indicator.keys() :
+        if isinstance(indicator[key],str) == True :
+            sub_key = None
+        if isinstance(indicator[key],dict) == True :
+            sub_key = list(indicator[key].keys())
+        keys[key] = sub_key
+    return keys
+
+
+
 measure_dimension_distance(ethiopia['ETH1'] , ethiopia['ETH3'] , 'icd10' , 10)
+
+
+
+
+
+
+get_indicator_dimension(ethiopia['ETH3'] )
